b2eafe053beb29875c0574a29fb4c4d94c615159
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-04-21 10:40:25 -0400

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = [] # not efficient to continuously update a string!
+                    # result gets copied every time, which results in O(n^2)!
+
+        i, j = 0, 0
+        while i < len(word1) and j < len(word2):
+            result.append(word1[i])
+            result.append(word2[j])
+            i += 1
+            j += 1
+        result.append(word1[i:])
+        result.append(word2[i:])
+
+        return "".join(result)
