8a697bb0c18c8c820b2c681ff9ce8b848569997c
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-04-27 23:00:45 -0400

    Update: time-based-key-value-store - submission-23

--- Data Structures & Algorithms/time-based-key-value-store/submission-23.py
+++ Data Structures & Algorithms/time-based-key-value-store/submission-23.py
@@ -1,9 +1,11 @@
 class TimeMap:
 
     def __init__(self):
-        self.tmap = defaultdict(list)
+        self.tmap = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.tmap:
+            self.tmap[key] = []
         self.tmap[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
@@ -11,7 +13,7 @@ class TimeMap:
             return ""
         
         values = self.tmap[key]
-        result = values[0][0]
+        result = ""
         l, r = 0, len(values) - 1
         while l <= r:
             m = (l + r) // 2
@@ -20,5 +22,4 @@ class TimeMap:
                 l = m + 1
             else:
                 r = m - 1
-        
         return result
