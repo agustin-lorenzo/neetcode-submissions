4531a030f723546d3ab725f6419b1a75e87a2285
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-04-30 00:07:25 -0400

    Add: search-for-word - submission-12

---
+++ Data Structures & Algorithms/search-for-word/submission-12.py
@@ -0,0 +1,28 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        R, C = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, i):
+            if i >= len(word):
+                return True
+            
+            if (r not in range(R) or 
+                c not in range(C) or
+                (r, c) in path or
+                board[r][c] != word[i]):
+                return False
+            
+            path.add((r, c))
+            result = (dfs(r + 1, c, i + 1) or
+                    dfs(r - 1, c, i + 1) or
+                    dfs(r, c + 1, i + 1) or
+                    dfs(r, c - 1, i + 1))
+            path.remove((r, c))
+            return result
+        
+        for r in range(R):
+            for c in range(C):
+                if dfs(r, c, 0):
+                    return True
+        return False
