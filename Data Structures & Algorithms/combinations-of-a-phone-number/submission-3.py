2fb941b0f97ef968ee39da666ccf67dd0d0c6b7e
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-04-29 22:28:28 -0400

    Add: combinations-of-a-phone-number - submission-3

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        d2c = {'2': 'abc',
+               '3': 'def',
+               '4': 'ghi',
+               '5': 'jkl',
+               '6': 'mno',
+               '7': 'pqrs',
+               '8': 'tuv',
+               '9': 'wxyz'}
+
+        result = []
+
+        def backtrack(i, combo):
+            if i == len(digits):
+                if combo:
+                    result.append("".join(combo))
+                return
+            
+            for c in d2c[digits[i]]:
+                combo.append(c)
+                backtrack(i + 1, combo)
+                combo.pop()
+            
+        backtrack(0, [])
+        return result
