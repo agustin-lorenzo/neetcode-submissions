4497192e2de4a13f12af88ac965140baf03a2869
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-05-06 09:52:26 -0400

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.py
@@ -0,0 +1,35 @@
+class TrieNode():
+    def __init__(self):
+        self.child = {}
+        self.end = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.child:
+                curr.child[c] = TrieNode()
+            curr = curr.child[c]
+        curr.end = True
+
+    def search(self, word: str) -> bool:
+        
+        def dfs(node, i):
+            if i >= len(word):
+                return node.end
+            
+            if word[i] == '.':
+                for c in node.child:
+                    if dfs(node.child[c], i + 1):
+                        return True
+            
+            if word[i] in node.child:
+                return dfs(node.child[word[i]], i + 1)
+            
+            return False
+        
+        return dfs(self.root, 0)
