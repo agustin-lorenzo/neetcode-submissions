4a4f79ad7956374b66f112edc5784373483eb011
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-07-15 13:26:00 -0400

    Add: minimum-window-with-characters - submission-15

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-15.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        result = ""
+        resultLen = float("inf")
+
+        window = {}
+        tmap = defaultdict(int)
+        for c in t:
+            tmap[c] += 1
+        need = len(tmap)
+        have = 0
+        
+        l = 0
+        for r in range(len(s)):
+            window[s[r]] = window.get(s[r], 0) + 1
+            if window[s[r]] == tmap[s[r]]:
+                have += 1
+            
+            while have == need:
+                if r - l + 1 < resultLen:
+                    result = s[l:r+1]
+                    resultLen = r - l + 1
+                
+                if window[s[l]] == tmap[s[l]]:
+                    have -= 1
+                window[s[l]] -= 1
+                l += 1
+        
+        return result
