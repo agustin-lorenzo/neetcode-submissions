c2c14b826e0df5cc076ce2c5ee2fbfc92961e090
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-07-16 15:28:47 -0400

    Add: lru-cache - submission-16

---
+++ Data Structures & Algorithms/lru-cache/submission-16.py
@@ -0,0 +1,41 @@
+class Node:
+
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+        self.head, self.tail = Node(0, 0), Node(0, 0)
+        self.head.prev, self.tail.next = self.tail, self.head
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+    
+    def insert(self, node):
+        prev, nxt = self.head.prev, self.head
+        prev.next, node.prev = node, prev
+        node.next, nxt.prev = nxt, node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.remove(self.cache[key])
+        self.insert(self.cache[key])
+        return self.cache[key].val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.tail.next
+            self.remove(lru)
+            del self.cache[lru.key]
