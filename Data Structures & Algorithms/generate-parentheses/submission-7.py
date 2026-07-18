40da55c55dee78602234140d68ba8e67da11ca97
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-07-18 11:02:00 -0400

    Add: generate-parentheses - submission-7

---
+++ Data Structures & Algorithms/generate-parentheses/submission-7.py
@@ -0,0 +1,26 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+        combo = []
+        def dfs(nOpen, nClose):
+            if nClose == n:
+                result.append("".join(combo))
+                return
+            
+            if nClose == n:
+                return
+            
+            if nOpen < n:
+                combo.append("(")
+                dfs(nOpen + 1, nClose)
+                combo.pop()
+            
+            if nClose < nOpen:
+                combo.append(")")
+                dfs(nOpen, nClose + 1)
+                combo.pop()
+            
+            
+        dfs(0, 0)
+        return result
