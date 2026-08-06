a6f5a1918757e6b570e483e06a9aace3a1c763a5
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-08-06 16:31:32 -0400

    Add: is-anagram - submission-36

---
+++ Data Structures & Algorithms/is-anagram/submission-36.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        smap, tmap = defaultdict(int), defaultdict(int)
+        for c in s:
+            smap[c] += 1
+        for c in t:
+            tmap[c] += 1
+        
+        return smap == tmap
