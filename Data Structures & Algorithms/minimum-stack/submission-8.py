5542684b9f56d6da7ea5dc831d3c13d1c4fa23ac
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-08-26 11:42:17 -0400

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.mins = []        
+
+    def push(self, val: int) -> None:
+        if not self.stack or val < self.mins[-1]:
+            self.mins.append(val)
+        else:
+            self.mins.append(self.mins[-1])
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.mins.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.mins[-1]
