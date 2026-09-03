7797445e612205e624754d50a78dbf5ea07d4a6f
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-09-03 14:50:55 -0400

    Add: valid-sudoku - submission-20

---
+++ Data Structures & Algorithms/valid-sudoku/submission-20.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        R, C = len(board), len(board[0])
+        rows, cols, blocks = defaultdict(set), defaultdict(set), defaultdict(set)
+
+        for r in range(R):
+            for c in range(C):
+                curr = board[r][c]
+                if curr == ".":
+                    continue
+                
+                if (curr in rows[r] or
+                    curr in cols[c] or
+                    curr in blocks[(r // 3, c // 3)]):
+                    return False
+                
+                rows[r].add(curr)
+                cols[c].add(curr)
+                blocks[(r // 3, c // 3)].add(curr)
+        
+        return True
