db439e5d35f69a46935bdbb86cf79b7a3e521767
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-09-08 09:54:05 -0400

    Add: permutation-string - submission-18

---
+++ Data Structures & Algorithms/permutation-string/submission-18.py
@@ -0,0 +1,21 @@
+from collections import Counter, defaultdict
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1map = Counter(s1)
+        window = defaultdict(int)
+        for i in range(len(s1) - 1):
+            window[s2[i]] += 1
+
+        l = 0
+        for r in range(len(s1) - 1, len(s2)):
+            window[s2[r]] += 1
+            if window == s1map:
+                return True
+            
+            window[s2[l]] -= 1
+            if window[s2[l]] == 0:
+                del window[s2[l]]
+            l += 1
+        
+        return False
