64a52ac0c68485787933f907cf4f37a3f5b9d0a1
Agustin Lorenzo <95234431+agustin-lorenzo@users.noreply.github.com>
2026-09-21 11:05:28 -0400

    Add: sort-an-array - submission-10

---
+++ Data Structures & Algorithms/sort-an-array/submission-10.py
@@ -0,0 +1,36 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+
+        def conquer(l, r):
+            m = (l + r) // 2
+            left, right = nums[l:m+1], nums[m+1:r+1]
+            i, j, k = l, 0, 0
+
+            while j < len(left) and k < len(right):
+                if left[j] < right[k]:
+                    nums[i] = left[j]
+                    j += 1
+                else:
+                    nums[i] = right[k]
+                    k += 1
+                i += 1
+            
+            while j < len(left):
+                nums[i] = left[j]
+                j += 1
+                i += 1
+            while k < len(right):
+                nums[i] = right[k]
+                k += 1
+                i += 1
+        
+        def divide(l, r):
+            if l >= r:
+                return
+            m = (l + r) // 2
+            divide(l, m)
+            divide(m + 1, r)
+            conquer(l, r)
+        
+        divide(0, len(nums) - 1)
+        return nums
